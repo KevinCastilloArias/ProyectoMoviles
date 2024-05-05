454e4a7026e5df76af0bfc06cab7a814d9253c88
MaizethC <maizethcisneros@gmail.com>
2024-05-05 12:06:14 -0600

    Mai

--- Proyecto/BackEnd/Entidades/Entidad/Articulo.cs
+++ Proyecto/BackEnd/Entidades/Entidad/Articulo.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BackEnd.Entidades.Entidad
+namespace BackEnd.Entidades
 {
     public class Articulo
     {
+        public int ID_ARTICULO { get; set; }
+        public string NOMBRE { get; set; }
+        public string DESCRIPCION { get; set; }
+        public int CANTIDAD_DISPONIBLE { get; set; }
+        public decimal PRECIO { get; set; }
+        public byte[] IMAGEN { get; set; }
+        public int ID_CATEGORIA { get; set; }
     }
 }
