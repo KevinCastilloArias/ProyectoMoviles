f6ce73c2d87e1099e8d4e6c190425240e60ef3a7
MaizethC <maizethcisneros@gmail.com>
2024-05-06 10:02:13 -0600

    Mai

--- Proyecto/BackEnd/Entidades/Entidad/Categoria.cs
+++ Proyecto/BackEnd/Entidades/Entidad/Categoria.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BackEnd.Entidades
+namespace BackEnd.Entidades.Entidad
 {
-    public class Roll
+    internal class Categoria
     {
     }
 }
