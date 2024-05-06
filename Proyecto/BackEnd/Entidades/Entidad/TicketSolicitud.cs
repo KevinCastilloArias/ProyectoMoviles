c932a3dd1ae51c3deeae4636836226e3f689dc61
MaizethC <maizethcisneros@gmail.com>
2024-05-06 10:35:35 -0600

    M

---
+++ Proyecto/BackEnd/Entidades/Entidad/TicketSolicitud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Entidades
+{
+    public class TicketSolicitud
+    {
+        public int ID_TICKET_SOLICITUD { get; set; }
+        public string ID_USUARIO { get; set; }
+        public string DETALLE {  get; set; }
+        public string FECHA_VISITA {  get; set; }
+        
+    }
+}
