2522f1379dc8f20d70727ec49e309f956733a1c1
MaizethC <maizethcisneros@gmail.com>
2024-05-20 15:29:51 -0600

    M

--- Proyecto/BackEnd/Entidades/Entidad/Usuario.cs
+++ Proyecto/BackEnd/Entidades/Entidad/Usuario.cs
@@ -8,5 +8,10 @@ namespace BackEnd.Entidades
 {
     public class Usuario
     {
+        public int ID_CORREO{ get; set; }
+        public string TELEFONO {get; set; }
+        public string CONTRASENA {  get; set; }
+        public int ID_ROL {  get; set; }
+        public int ACTIVO { get; set; }
     }
 }
